6376600dfb7ef006eb6b75be4d02f03d59ef0ba9
aelim0409 <72659915+aelim0409@users.noreply.github.com>
2021-07-08 01:20:51 +0900

    카드뷰포함 메인화면

--- app/src/main/java/com/example/photodiaryapp/MainActivity.java
+++ app/src/main/java/com/example/photodiaryapp/MainActivity.java
@@ -1,14 +1,61 @@
 package com.example.photodiaryapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
+import android.animation.ArgbEvaluator;
 import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    ViewPager viewPager;
+    Adapter adapter;
+    List<Model> models;
+    Integer[] colors = null;
+    ArgbEvaluator argbEvaluator =new ArgbEvaluator();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        models = new ArrayList<>();
+        models.add(new Model(R.drawable.sky, "Sky"));
+        models.add(new Model(R.drawable.sky, "sky2"));
+
+        adapter = new Adapter(models, this);
+
+        viewPager=findViewById(R.id.view_pager);
+        viewPager.setAdapter(adapter);
+        viewPager.setPadding(130,0,130,0);
+
+        Integer[] colors_temp = {getResources().getColor(R.color.transparent),
+                getResources().getColor(R.color.transparent)};
+        colors=colors_temp;
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                if(position<(adapter.getCount()-1)&&position<(colors.length-1))
+                {
+                    viewPager.setBackgroundColor((Integer)argbEvaluator.evaluate(positionOffset,colors[position],colors[position+1]));
+                }else
+                {
+                    viewPager.setBackgroundColor(colors[colors.length-1]);
+                }
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
     }
 }
